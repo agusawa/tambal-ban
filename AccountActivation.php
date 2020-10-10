494d676af207f65a3f151f9a7ea36baac91301ec
subhan nooriansyah <subhan.nooriansyah@gmail.com>
2020-10-10 15:41:05 +0700

    revision and clue alogrithm coding

--- AccountActivation.php
+++ AccountActivation.php
@@ -1,7 +1,16 @@
 <?php
+
+// process activation account
+// check name --> function return boolean
+// check email --> function return boolean
+// generate token (md5) --> function string
+// activation status --> return boolean ==> account is activate or not
+// class registration (input) --> checking name and email use a class validation then, use a class activation (proses) --> store name, email and token in cookies/txt/database (output)
+// class login (input) --> use a class validation for checking name, email and token (proses) --> main menu (output)
+
 class ActivateAccount {
-	public function activate()
-    {
+
+	public function activate() {
 		$available_email = 'abcd99@gmail.com';
 		$new_email = 'sayariri1@gmail.com';
         $available_token = '01ABCD';
