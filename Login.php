7501c7f213680707bfa173d55f4283cd32f08383
dini212 <dwirahmadini1234@gmail.com>
2020-10-10 10:41:05 +0700

    Login

--- Login.php
+++ Login.php
@@ -1,16 +1,29 @@
 <?php
-class Login{
-	public Function isValid()
-    {
-    //$Email = 'dwirahmadini212@gmail.com';
-    $Email = 'dini123@gmail.com';
-    $Password = 'SistemInformasi19';
-    if($Email == 'dwirahmadini212@gmail.com' AND $Password == 'SistemInformasi19'){
-    echo "Login sukses!";
-    }else{
-    echo "email atau password yang anda masukkan salah!";
-    }}}
-    
-    $Valid = new Login();
-    $Valid -> isValid();
+class login {
+	private $correctEmail = "peter@example.com";
+	private $correctPassword = "peter1234";
+
+	protected $inputEmail;
+	protected $inputPassword;
+
+	public function __construct($inputEmail, $inputPassword) {
+		$this->inputEmail = $inputEmail;
+		$this->inputPassword = $inputPassword;
+	}
+
+	public function isValid() {
+		if($this->inputEmail === $this->correctEmail AND $this->inputPassword === $this->correctPassword) {
+			echo "Login succesful!";
+		} else {
+			echo "wrong password or email!";
+		}
+	}
+}
+
+$inputEmail = "peter@example.com";
+$inputPassword = "peter1234";
+
+$login = new login($inputEmail, $inputPassword);
+$login ->isValid();
+
 ?>
