3e5472a76f41783a56d13a9b6c150dcc65a496e6
dini212 <dwirahmadini1234@gmail.com>
2020-10-17 02:16:05 +0700

    ChangeProfileInformation

--- ChangeProfileInformation.php
+++ ChangeProfileInformation.php
@@ -1,25 +1,29 @@
 <?php
 
 class ChangeProfileInformation {
-	private $listId = "abe43";
-	private $listEmail = "abe@example.com";
+	private $correctId = "abe43";
+	private $registeredEmail = "abe@example.com";
 
 	protected $inputId;
 	protected $inputEmail;
+	protected $inputName;
 
-	public function __construct($inputId, $inputEmail) {
+	public function __construct($inputId, $inputEmail, $inputName) {
 		$this->inputEmail = $inputEmail;
 		$this->inputId = $inputId;
+		$this->inputName = $inputName;
 	}
 
-	public function save() {
-		if($this->inputId === $this->listId) {
+	public function isValidId() {
+		if($this->inputId === $this->correctId) {
 			echo "that id have been used, please use another id. <br/>";
 		} else {
 			echo "success, your id have been register. <br/>";
 		}
+	}
 
-		if($this->inputEmail === $this->listEmail) {
+	public function isRegisteredEmail() {
+		if($this->inputEmail === $this->registeredEmail) {
 			echo "that email have already use, please use another email. <br/>";
 		} else {
 			echo "succes, your email have been register. <br/>";
@@ -29,8 +33,10 @@ class ChangeProfileInformation {
 
 $inputEmail = "dwirahma@example.com";
 $inputId = "rahmadini234";
+$inputName = "Howard";
 
-$changeProfileInformation = new ChangeProfileInformation($inputId, $inputEmail);
-$changeProfileInformation -> save();
+$changeProfileInformation = new ChangeProfileInformation($inputId, $inputEmail, $inputName);
+$changeProfileInformation -> isValidId();
+$changeProfileInformation -> isRegisteredEmail();
 
 ?>
