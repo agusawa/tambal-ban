16c38885e4f9a9c4dc90ef6e3756575cdacbe434
auliacahyar <auliacahyar.27@gmail.com>
2020-10-29 15:49:36 +0700

    Request

---
+++ Request.php
@@ -0,0 +1,27 @@
+<?php
+class Request {
+	private $params;
+    private $inputs;
+    
+    public function __construct() {
+    	$this->params = $_GET;
+        $this->inputs = $_POST;
+    }
+    
+    public function input($key) {
+    	if (array_key_exists($key, $this->inputs)) {
+        	return $this->inputs[$key];
+       } else {
+       		return null;
+        }
+    }
+    
+    public function param($key) {
+    	if (array_key_exists($key, $this->params)) {
+        	return $this->params[$key];
+        } else {
+        	return null;
+        }
+    }
+}
+?>
