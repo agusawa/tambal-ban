380eec6bb793257def73c8dbd80c8857f5fe45e2
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-10-30 15:05:52 +0700

    Merapikan kodingan Request.php

--- Request.php
+++ Request.php
@@ -1,27 +1,26 @@
 <?php
 class Request {
-	private $params;
+    private $params;
     private $inputs;
-    
+
     public function __construct() {
-    	$this->params = $_GET;
+        $this->params = $_GET;
         $this->inputs = $_POST;
     }
-    
+
     public function input($key) {
-    	if (array_key_exists($key, $this->inputs)) {
-        	return $this->inputs[$key];
-       } else {
-       		return null;
+        if (array_key_exists($key, $this->inputs)) {
+            return $this->inputs[$key];
+        } else {
+            return null;
         }
     }
-    
+
     public function param($key) {
-    	if (array_key_exists($key, $this->params)) {
-        	return $this->params[$key];
+        if (array_key_exists($key, $this->params)) {
+            return $this->params[$key];
         } else {
-        	return null;
+            return null;
         }
     }
 }
-?>
