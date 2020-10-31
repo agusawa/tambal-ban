68e520f3b6900722ff752834a35e0e8437e986b7
dini212 <dwirahmadini1234@gmail.com>
2020-10-31 13:13:18 +0700

    TirePatch

---
+++ Models/TirePatch.php
@@ -0,0 +1,54 @@
+<?php
+
+class TirePatch {
+	private $id;
+	private $name;
+	private $description;
+	private $address;
+	private $whatsapp;
+	private $accountId;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getDescription() {
+		return $this->email;
+	}
+
+	public function getAddress() {
+		return $this->address;
+	}
+
+	public function getWhatsapp() {
+		return $this->whatsapp;
+	}
+
+	public function getAccountId() {
+		return $this->accountId;
+	}
+
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+
+	public function setAddress($address) {
+		$this->address = $address;
+	}
+
+	public function setWhatsapp($whatsapp) {
+		$this->whatsapp = $whatsapp;
+	}
+
+	public function setAccountId($accountId) {
+		$this->accountId = $accountId;
+	}
+}
