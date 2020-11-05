e27f760ba1d7bbfd6315c02004a4d29c5f6825b9
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-11-05 10:24:30 +0700

    Menambahkan properti request, method handle, method get, dan juga method post.

--- Core/View.php
+++ Core/View.php
@@ -1,10 +1,35 @@
 <?php
 
+require __DIR__ . "/Request.php";
+
 abstract class View {
     protected $method;
+    protected $request;
 
     public function __construct() {
         $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->request = new Request();
+
+        $this->handle();
+    }
+
+    private function handle() {
+        switch ($this->method) {
+            case "POST":
+                $this->post();
+                break;
+            default:
+                $this->get();
+                break;
+        }
+    }
+
+    protected function get() {
+        // Should return 404 error.
+    }
+
+    protected function post() {
+        // Should return 404 error.
     }
 
     protected function redirect($location) {
