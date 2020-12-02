3be1a42d1dd5176c510660b09ffb5b31f4e34a7d
auliacahyar <auliacahyar.27@gmail.com>
2020-12-02 19:10:49 +0700

    Database

---
+++ AccountActivation.php
@@ -0,0 +1,35 @@
+<?php
+
+class ActivateAccount {
+	private $availableEmail = 'abcd99@gmail.com';
+    private $availableToken = '01ABCD';
+    
+    protected $inputEmail;
+    protected $inputToken;
+    
+	public function __construct($inputEmail, $inputToken) {
+		$this->inputEmail = $inputEmail;
+		$this->inputToken = $inputToken;
+    }
+    
+    public function isValid() {
+		if ($this->inputEmail === $this->availableEmail AND $this->inputToken === $this->availableToken) {
+			echo "Sukses";
+		} else {
+			echo "Gagal";
+		}
+    }
+    protected function get() {
+        $this->render("ActivateAccount");
+    }
+
+    protected function post() {    	
+    }
+}
+
+$inputEmail = "abcd99@gmail.com";
+$inputToken = "01ABCD";
+
+$activateAccount = new ActivateAccount($inputEmail, $inputToken);
+$activateAccount->isValid();
+?> 
---
+++ rani.sql
@@ -0,0 +1,41 @@
+-- drop 'blog' database if exists.
+DROP DATABASE IF EXISTS 'tambalBan';
+
+-- create 'tambalBan' database.
+CREATE DATABASE 'tambalBan';
+
+-- use 'tambalBan' database.
+USE 'tambalBan';
+
+-- drop 'user' table if exists.
+DROP TABLE IF EXISTS 'user';
+
+-- create 'user' table.
+CREATE TABLE 'user' (
+	'id' INT(10) NOT NULL AUTO_INCREMENT, 
+	'name' VARCHAR(50) NOT NULL,
+	'email' VARCHAR(50) NOT NULL UNIQUE,
+	'password' VARCHAR(50) NOT NULL,
+	'created' INT(10) NOT NULL,
+	'updated' INT(10) NULL,
+	PRIMARY KEY ('id')
+	INDEX('email')
+	)ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+-- drop 'tire_patches' table if exists.
+DROP TABLE IF EXISTS 'tire_patches';
+
+-- create 'tire_patches' table.
+CREATE TABLE 'tire_patches' (
+	'id' INT(10) NOT NULL AUTO_INCREMENT,
+	'user_id' CHAR(10) NOT NULL,
+	'name' VARCHAR(50) NOT NULL,
+	'address' VARCHAR(255) NOT NULL,
+	'description' TEXT NULL,
+	'picture' VARCHAR(255) NULL,
+	'whatsapp_number' VARCHAR(15) NOT NULL,
+	'created' INT(10) NOT NULL,
+	'updated' INT(10) NULL,
+	PRIMARY KEY ('id')
+	FOREIGN KEY ('user_id') REFERENCES 'user'('id') ON DELETE CASCADE
+	)ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
