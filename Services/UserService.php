4f3f06918900ea91455d8593b10ff6a277e85e90
dini212 <dwirahmadini1234@gmail.com>
2020-12-09 22:46:28 +0700

    week 11

--- Services/UserService.php
+++ Services/UserService.php
@@ -3,4 +3,16 @@
 require __DIR__ . "/../Core/Service.php";
 
 class UserService extends Service {
+
+	public static function findOneByEmail($email) {
+		$stmt = $this->connection->prepare("SELECT * FROM 'users' WHERE 'email' = ?");
+
+		$stmt->bind_param('s', $email);
+
+		$stmt->execute();
+
+		return $stmt->get_result();
+	}
 }
+$email = 'mari@example.com';
+var_dump(UserService::findOneByEmail($email));
