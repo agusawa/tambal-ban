49b8d451142a8def1c8ee41a07acf12cbf9b0cfb
auliacahyar <auliacahyar.27@gmail.com>
2020-12-09 21:34:33 +0700

    Tugas ke 11

--- Services/TirePatchService.php
+++ Services/TirePatchService.php
@@ -3,4 +3,17 @@
 require __DIR__ . "/../Core/Service.php";
 
 class TirePatchService extends Service {
+    
+    public static function search($keyword) {
+
+        $stmt = $this->connection->prepare("SELECT * FROM 'tire_patches' WHERE 'address' = ?");
+
+        $stmt->bind_param("s", $keyword);
+
+        $stmt->execute();
+        
+        return $stmt->get_result();
+    }
 }
+$keyword = 'contoh@example.com';
+var_dump(TirePatchService::search($email));
