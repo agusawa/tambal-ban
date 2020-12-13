41918f9c5e2ec07de597cf303ccd93c915dd0eda
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-12-13 20:31:40 +0700

    Menambahkan method findOneById dan delete pada UserService

--- Services/UserService.php
+++ Services/UserService.php
@@ -36,7 +36,39 @@ class UserService extends Service {
         $stmt->close();
         return false;
 
-	}
+    }
+
+    public static function findOneById($id) {
+        $stmt = self::$connection->prepare("SELECT * FROM users WHERE `id` = ? LIMIT 1");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $user = $stmt->get_result()[0];
+
+        $stmt->close();
+        if ($user) return $user;
+        return null;
+    }
+    
+    public static function delete($id) {
+        $user = self::findOneById($id);
+
+        if (!$user) return false;
+
+        $stmt = self::$connection->prepare("DELETE users WHERE `id` = ? LIMIT 1");
+        $stmt->bind_param("i", $id);
+        $process = $stmt->execute();
+
+        // Jika proses sukses.
+        if ($process) {
+            $stmt->close();
+            return true;
+        }
+
+        // Jika proses gagal.
+        $stmt->close();
+        return false;
+    }
 }
 
 require __DIR__ . "/../Models/User.php";
@@ -57,3 +89,6 @@ require __DIR__ . "/../Models/User.php";
 
 $email = 'mari@example.com';
 var_dump(UserService::findOneByEmail($email));
+
+$id = 1;
+var_dump(UserService::delete($id));
