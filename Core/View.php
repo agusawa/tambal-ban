e080f33cf33b93c893aa80a1683db78ac7f42174
Agus Stiawan <agus.stiawan2211@gmail.com>
2020-12-22 10:09:16 +0700

    Menambahkan docstring dan method getDefaultExtraVariables

--- Core/View.php
+++ Core/View.php
@@ -1,12 +1,27 @@
 <?php
 
+require __DIR__ . "/Helpers/Session.php";
 require __DIR__ . "/Helpers/Request.php";
 
 abstract class View
 {
+    /**
+     * Method of request, GET or POST.
+     *
+     * @var string
+     */
     protected $method;
+
+    /**
+     * The request instance
+     *
+     * @var \Core\Helpers\Request
+     */
     protected $request;
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         $this->method = $_SERVER['REQUEST_METHOD'];
@@ -15,6 +30,25 @@ abstract class View
         $this->handle();
     }
 
+    /**
+     * Get array of default variable for template.
+     *
+     * @return array
+     */
+    private function getDefaultExtraVariable()
+    {
+        return [
+            'request' => $this->request,
+            'error' => Session::getError(),
+            'success' => Session::getSuccess(),
+        ];
+    }
+
+    /**
+     * Select whether to enter the GET or POST method.
+     *
+     * @return void
+     */
     private function handle()
     {
         switch ($this->method) {
@@ -27,23 +61,54 @@ abstract class View
         }
     }
 
+    /**
+     * Function to be called when the GET method occurs.
+     * 
+     * @return void
+     */
     abstract protected function get();
+
+    /**
+     * Function to be called when the POST method occurs.
+     *
+     * @return void
+     */
     abstract protected function post();
 
+    /**
+     * Redirect to URL location.
+     *
+     * @param string $location
+     * @return void
+     */
     protected function redirect($location)
     {
         header("Location: $location");
     }
 
+    /**
+     * Declare variables and render the HTML file.
+     *
+     * @param string $file Filename in Templates folder
+     * @param array $extra
+     * @return void
+     */
     protected function render($file, $extra = [])
     {
-        foreach ($extra as $key => $value) {
+        $extraVariables = array_merge($this->getDefaultExtraVariable(), $extra);
+
+        foreach ($extraVariables as $key => $value) {
             ${$key} = $value;
         }
 
         require __DIR__ . "/../Templates/$file";
     }
 
+    /**
+     * Show http 404 not found page.
+     *
+     * @return void
+     */
     protected function notFound()
     {
         $this->render("404.php");
