5093dd946b657a2d6621ffff9be87bea7a16cca8
Nabilara <nabilaturrahma@yahoo.com>
2020-12-22 11:23:03 +0700

    menambahkan docstring

--- Views/Signup.php
+++ Views/Signup.php
@@ -1,18 +1,4 @@
 <?php
-// revision 8 oct 2020: 
-// 1. coding using english word
-// 2. choose typing CamelCase or Snake Case or Pascal Case or Normal Case
-// 3. no. 2 should consistent
-// 4. style curl brace ==> {}, example 
-// 4.1 function() {
-//     }
-// 4.2 function()
-//     {
-//     }
-// 5. choose no. 4.1 or no. 4.2
-// 6. choose the right data type ==> char ('') or string("")
-// 7. before create login and registration is used to create a account firstly
-// 8. example basic php in file FindPlace.php
 
 require __DIR__ . "/../Core/View.php";
 
