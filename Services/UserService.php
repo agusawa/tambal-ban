31d527860fd512201c7f94f9a5ef19132d72f91b
Nabilara <nabilaturrahma@yahoo.com>
2020-12-23 20:54:19 +0700

    mengubah petik satu menjadi backtick

--- Services/UserService.php
+++ Services/UserService.php
@@ -23,7 +23,7 @@ class UserService extends Service
 
     public static function insert($user)
     {
-        $stmt = self::getConnection()->prepare("INSERT INTO 'users' ('name', 'email', 'password', 'created') VALUES (?, ?, ?, ?)");
+        $stmt = static::getConnection()->prepare("INSERT INTO `users` (`name`, `email`, `password`, `created`) VALUES (?, ?, ?, ?)");
 
         $name = $user->getName();
         $email = $user->getEmail();
@@ -37,12 +37,10 @@ class UserService extends Service
 
         // Jika proses sukses.
         if ($process) {
-            $stmt->close();
             return true;
         }
 
         // Jika proses gagal.
-        $stmt->close();
         return false;
     }
 
@@ -80,25 +78,3 @@ class UserService extends Service
         return false;
     }
 }
-
-require __DIR__ . "/../Models/User.php";
-$user = new User();
-$user->setName("John Doe");
-$user->setEmail("johndoe@example.com");
-$user->setPassword("secret");
-$user->setCreated(time());
-
-$process = UserService::insert($user);
-
-// Jika proses sukses.
-if ($process) {
-    print_r("Process successful.");
-} else {
-    print_r("Process failure.\n");
-}
-
-$email = 'mari@example.com';
-var_dump(UserService::findOneByEmail($email));
-
-$id = 1;
-var_dump(UserService::delete($id));
