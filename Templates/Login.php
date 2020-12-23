b254ed9d04980acb40e7ea98c72fa84170c90136
dini212 <dwirahmadini1234@gmail.com>
2020-12-23 19:16:41 +0700

    menambahkan @var pada TirePatch.php dan membuat form pada template/login.php

--- Templates/Login.php
+++ Templates/Login.php
@@ -5,5 +5,11 @@
 </head>
 <body>
 	<h1> Welcome Login </h1>
+
+	<form action="" method = "POST">
+		<input type = "email" name = "email" placeholder="Name"> <br>
+		<input type = "password" name = "password" placeholder="Password"> <br>
+		<button type = "submit">Submit</button>
+	</form>
 </body>
 </html>
