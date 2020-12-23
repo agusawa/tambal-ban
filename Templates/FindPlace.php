a79139ff771dc2d713495decaba8761d1ccc3711
auliacahyar <auliacahyar.27@gmail.com>
2020-12-23 14:45:46 +0700

    Memperbaiki Find Place

--- Templates/FindPlace.php
+++ Templates/FindPlace.php
@@ -6,6 +6,25 @@
     <title>Find Place</title>
 </head>
 <body>
-    <h1>Hello Find Place</h1>
+    <h1>Find Place</h1>
+    
+    <form action="">
+        <input type="text" name="keyword" value="<?= $request->param("keyword")?> placeholder="Search ...">
+        <button type="submit">Search</button>
+    </form>
+
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>whatsaapNumber</th>
+        </tr>
+        <?php foreach ($tirepatches as $tirepatch); ?>
+        <tr>
+            <td><?= $tirepatch->$getName() ?></td>
+            <td><?= $tirepatch->$getAddress() ?></td>
+            <td><?= $tirepatch->$getwhatsaapNumber() ?></td>
+        </tr>
+    </table>
 </body>
 </html>
