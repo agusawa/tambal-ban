7b022f54d4c5d17201df6f71d01349142007b846
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-01-07 10:14:19 +0700

    Menghapus fole yang tidak dibutuhkan

--- app/Templates/AddTirePatch.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1> Butuh tambal </h1>
-</body>
-</html>
--- app/Templates/DeleteTirePatch.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Delete tire patch page</h1>
-</body>
-</html>
