294091fff2621eb57445dcd7a13dc4a69d5fcaa5
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-01-11 01:35:54 +0700

    Menambahkan class helpers Auth

---
+++ app/Core/Helpers/Auth.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . "/Session.php";
+require_once __DIR__ . "/../../Services/UserService.php";
+
+class Auth
+{
+    /**
+     * Holds current user data. So as not to perform continuous queries.
+     *
+     * @var User
+     */
+    private static $user;
+
+    /**
+     * Determine if the current user is authenticated.
+     *
+     * @return boolean
+     */
+    public static function check()
+    {
+        if (self::getUser() === null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return User
+     */
+    public static function getUser()
+    {
+        if (!self::$user) {
+            $userId = Session::get('userId');
+            self::$user = UserService::findOneById($userId);
+        }
+
+        return self::$user;
+    }
+}
